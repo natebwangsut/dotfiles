3e4b1001e17cc720483b05b0fc7a75ed4a0446cf
natebwangsut <nate.bwangsut@gmail.com>
2022-11-08 01:05:07 +0700

    fix: remove neoscroll.nvim

--- .config/nvchad/custom/plugins/init.lua
+++ .config/nvchad/custom/plugins/init.lua
@@ -1,11 +1,2 @@
 return {
-  ["karb94/neoscroll.nvim"] = {
-     config = function()
-        require("neoscroll").setup()
-     end,
-     -- lazy loading
-     setup = function()
-       nvchad.packer_lazy_load "neoscroll.nvim"
-     end,
-  },
 }
